b8472bf612629e1a90c83ba11428b366f93f849c
s.levchenko <sergey.levchenko@stoloto.ru>
2018-04-13 17:26:51 +0300

    fix

--- src/test/java/ru/gamble/stepdefs/CommonStepDefs.java
+++ src/test/java/ru/gamble/stepdefs/CommonStepDefs.java
@@ -2,6 +2,8 @@ package ru.gamble.stepdefs;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,11 +12,26 @@ import ru.sbtqa.tag.pagefactory.PageFactory;
 import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
 import ru.sbtqa.tag.pagefactory.exceptions.PageException;
 import ru.sbtqa.tag.pagefactory.exceptions.PageInitializationException;
+import ru.sbtqa.tag.pagefactory.drivers.TagWebDriver;
+
+//import static ru.sbtqa.tag.pagefactory.drivers.TagWebDriver.webDriver;
 
 @Slf4j
 public class CommonStepDefs {
 
     private static final Logger LOG = LoggerFactory.getLogger(CommonStepDefs.class);
+    private static WebDriver webDriver;
+
+
+    @ActionTitle("устанавливаем размер окна браузера")
+    public static void setSizeWindowBrowser(String param){
+            String[] size = param.split("x");
+            int width = Integer.parseInt(size[0]);
+            int height = Integer.parseInt(size[1]);
+        WebDriver driver =  TagWebDriver.getDriver();
+        webDriver.manage().window().setSize(new Dimension(width, height));
+
+    }
 
 
     public static void pressButton(String param){
