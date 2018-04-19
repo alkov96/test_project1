872798c3a7d837afaa990b9fc6861d5503dae7b8
Jenkins <jenkins@stoloto.ru>
2018-04-19 16:28:48 +0300

    changeBrowser

--- src/test/java/ru/gamble/CucumberTest.java
+++ src/test/java/ru/gamble/CucumberTest.java
@@ -14,9 +14,13 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.sbtqa.tag.pagefactory.PageFactory;
+import ru.sbtqa.tag.pagefactory.drivers.TagWebDriver;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true,
@@ -24,15 +28,25 @@ import ru.sbtqa.tag.pagefactory.PageFactory;
         glue = {"ru.gamble.stepdefs", "ru.sbtqa.tag.pagefactory.stepdefs"},
         features = {"src/test/resources/features/"},
         plugin= {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
-        tags = {"@smoke"})
+        tags = {"@1"})
 
 public class CucumberTest {
     private static final Logger LOG = LoggerFactory.getLogger(CucumberTest.class);
 
     @BeforeClass
     public static void setUp() {
-        PageFactory.getWebDriver().manage().window().setSize(new Dimension(1920, 1080));
+        TagWebDriver.getDriver().manage().window().setSize(new Dimension(1920, 1080));
 //        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("start-maximized");
+//        WebDriver driver = new ChromeDriver(options);
+////        System.setProperty("webdriver.chrome.driver", "webdrivers\\chromedriver.exe");
+//        PageFactory.getWebDriver().manage().window().setSize(new Dimension(1920, 1080));
+//
+//
+//
+//        driver.navigate().to("https://google.com");
+////        ChromeOptions options = new ChromeOptions();
 //        options.addArguments("--headless");
 //        options.addArguments("start-maximized");
 //        WebDriver driver = new ChromeDriver(options);
