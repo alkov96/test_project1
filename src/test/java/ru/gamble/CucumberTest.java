eb5351676377f7c016190a4f097bbdb0abdf4c19
a.kovtun <alina.kovtun@stoloto.ru>
2018-05-15 16:10:45 +0300

    слияние

--- src/test/java/ru/gamble/CucumberTest.java
+++ src/test/java/ru/gamble/CucumberTest.java
@@ -30,7 +30,7 @@ import java.util.concurrent.TimeUnit;
         glue = {"ru.gamble.stepdefs", "ru.sbtqa.tag.pagefactory.stepdefs"},
         features = {"src/test/resources/features/"},
         plugin= {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
-        tags = {"@smoke"})
+        tags = {"@DayEventsCoupon"})
 
 public class CucumberTest {
     private static final Logger LOG = LoggerFactory.getLogger(CucumberTest.class);
