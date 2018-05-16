d33de74454a958b65cd30276b9f0548dafaad6a7
Tatiana Zavaliy <tatiana.zavaliy@stoloto.ru>
2018-05-16 16:07:07 +0300

    переписала немного ункцию открытия главной страницы. теперь можно указывать закпуск на site1 или site2. по умолчанию - site2

--- src/test/java/ru/gamble/stepdefs/CommonStepDefs.java
+++ src/test/java/ru/gamble/stepdefs/CommonStepDefs.java
@@ -1,6 +1,7 @@
 package ru.gamble.stepdefs;
 
 import cucumber.api.DataTable;
+import cucumber.api.java.ru.Дано;
 import cucumber.api.java.ru.Когда;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -8,10 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.gamble.utility.BeforeTest;
 import ru.gamble.utility.DBUtils;
 import ru.sbtqa.tag.datajack.Stash;
-import ru.sbtqa.tag.datajack.exceptions.DataException;
 import ru.sbtqa.tag.pagefactory.Page;
 import ru.sbtqa.tag.pagefactory.PageFactory;
 import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
@@ -26,7 +25,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -37,7 +35,6 @@ public class CommonStepDefs extends GenericStepDefs {
 
     @ActionTitle("нажимает на кнопку")
     public static void pressButton(String param){
-        WebDriver driver = PageFactory.getWebDriver();
         Page page = null;
         WebElement button = null;
         try {
@@ -49,13 +46,8 @@ public class CommonStepDefs extends GenericStepDefs {
             LOG.error(e1.getMessage());
         }
         if(button.isDisplayed()){
-
-            // Код который пытается ещё раз нажать на кнопку, если WindowHandle не сменился после нажатия
-            String currentHandle = driver.getWindowHandle();
-            String newHandle = "";
-            LOG.info("Текущий идентификатор страницы::" + currentHandle);
-            button.click();
-            workWithPreloader();
+        button.click();
+        workWithPreloader();
         }else {
             LOG.error("ОШИБКА! Кнопка невидима.");
         }
@@ -93,8 +85,19 @@ public class CommonStepDefs extends GenericStepDefs {
 
     // Метод перехода на главную страницу
     @Когда("^переходит на главную страницу$")
-    public static void goToMainPage(){
-        PageFactory.getWebDriver().get(Props.get("webdriver.starting.url"));
+    public static void goToMainPage(){goToMainPage("site2");}
+
+    @Когда("^переходит на главную страницу '(.+)'$")
+    public static void goToMainPage(String site){
+        switch (site){
+            case "site1":
+                PageFactory.getWebDriver().get(Props.get("webdriver.starting.url"));
+                break;
+            default:
+                PageFactory.getWebDriver().get(Props.get("webdriver.starting.url2"));
+                break;
+        }
+
     }
 
     @Когда("^сохраняем в память таблицу$")
