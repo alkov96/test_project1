a8418758c43450eb2dfd4727c21d0dc446b88ea0
Sivak Pavel <pavel.sivak@stoloto.ru>
2018-05-18 16:37:32 +0300

    Добавил страницы админки

---
+++ src/test/java/ru/gamble/pages/AdministrationPages/servicesPages/ServiceSubMenu.java
@@ -0,0 +1,39 @@
+package ru.gamble.pages.AdministrationPages.servicesPages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.gamble.pages.AbstractPage;
+import ru.gamble.pages.AdministrationPages.TopMenu;
+import ru.sbtqa.tag.pagefactory.PageFactory;
+import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
+import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
+import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
+
+/**
+ * @author p.sivak.
+ * @since 18.05.2018.
+ */
+@PageEntry(title = "подменю Сервисы")
+public class ServiceSubMenu extends AbstractPage{
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceSubMenu.class);
+
+    @FindBy(xpath = "//div[@id='tabbar-1483-innerCt']")
+    private WebElement subMenu;
+
+    @ElementTitle("Сервисные сообщения")
+    @FindBy(xpath = "//span[@id='tab-1618-btnInnerEl']")
+    private WebElement serviceMessages;
+
+    public ServiceSubMenu() {
+        WebDriver driver = PageFactory.getDriver();
+        PageFactory.initElements(new HtmlElementDecorator(
+                new HtmlElementLocatorFactory(driver)), this);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(subMenu));
+    }
+}
