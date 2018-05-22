1c40cf509940a3d369c7eb447f2d3d5b1b855815
Sivak Pavel <pavel.sivak@stoloto.ru>
2018-05-22 09:32:27 +0300

    Добавлены страницы профиля: бонусы, сообщения, настройка уведомлений, история операций, профиль

--- src/test/java/ru/gamble/pages/userProfilePages/BonusesPage.java
+++ src/test/java/ru/gamble/pages/userProfilePages/BonusesPage.java
@@ -9,29 +9,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.gamble.pages.AbstractPage;
 import ru.sbtqa.tag.pagefactory.PageFactory;
-import ru.sbtqa.tag.pagefactory.annotations.ElementTitle;
 import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
+/**
+ * @author p.sivak.
+ * @since 21.05.2018.
+ */
+@PageEntry(title = "Бонусы в кабинете")
+public class BonusesPage extends AbstractPage{
+    private static final Logger LOG = LoggerFactory.getLogger(BonusesPage.class);
 
-@PageEntry(title = "Мини Личный Кабинет")
-public class PopUPLCPage extends AbstractPage {
-    private static final Logger LOG = LoggerFactory.getLogger(PopUPLCPage.class);
+    @FindBy(xpath = "//table[@class='table table_bonus']")
+    private WebElement table_bonus;
 
-    @ElementTitle("адрес почты")
-    @FindBy(id ="user-profile")
-    private WebElement emailBotton;
-
-    @ElementTitle("Выйти")
-    @FindBy(id ="log-out-button")
-    private WebElement exitButton;
-
-    public PopUPLCPage() {
+    public BonusesPage() {
         WebDriver driver = PageFactory.getDriver();
         PageFactory.initElements(new HtmlElementDecorator(
                 new HtmlElementLocatorFactory(driver)), this);
-        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(exitButton));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(table_bonus));
     }
-
 }
