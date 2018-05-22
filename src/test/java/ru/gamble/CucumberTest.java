2abce1ec8a5f4a1fec25a520c11a9c3a49e8707e
a.kovtun <alina.kovtun@stoloto.ru>
2018-05-22 15:53:15 +0300

    CouponAcceptChange_C1048

--- src/test/java/ru/gamble/CucumberTest.java
+++ src/test/java/ru/gamble/CucumberTest.java
@@ -30,7 +30,7 @@ import java.util.concurrent.TimeUnit;
         glue = {"ru.gamble.stepdefs", "ru.sbtqa.tag.pagefactory.stepdefs"},
         features = {"src/test/resources/features/"},
         plugin= {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
-        tags = {"@smoke"})
+        tags = {"@CouponAcceptChange_C1048"})
 
 public class CucumberTest {
     private static final Logger LOG = LoggerFactory.getLogger(CucumberTest.class);
