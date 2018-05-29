c96baf8c62ba72fa3b66a9bd00b874987e620797
Tatiana Zavaliy <tatiana.zavaliy@stoloto.ru>
2018-05-29 15:53:42 +0300

    добавила очистку купона

--- src/test/java/ru/gamble/pages/AbstractPage.java
+++ src/test/java/ru/gamble/pages/AbstractPage.java
@@ -91,6 +91,10 @@ public abstract class AbstractPage extends Page {
     @FindBy(id = "quickbet")
     protected WebElement quickButton;
 
+    @ElementTitle("Очистить всё")
+    @FindBy(xpath = "//span[@class='coupon-clear-all__text ng-binding']")
+    private WebElement clearCoupon;
+
 //для ставок экспресс, быстрой ставки - т.е. там где 1 поле для ставки
     @ElementTitle("поле суммы общей ставки")
     @FindBy(id="express-bet-input")
@@ -411,6 +415,13 @@ public abstract class AbstractPage extends Page {
     @ActionTitle("ждет некоторое время")
     public void waiting(String sec) throws InterruptedException {
         Thread.sleep(Integer.valueOf(sec)*1000);
+    }
+
+    @ActionTitle("очищает купон")
+    public void clearCoupon(){
+        if (clearCoupon.isDisplayed()) clearCoupon.click();
+        WebDriver driver = PageFactory.getDriver();
+        new WebDriverWait(driver,10).until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//span[@class='coupon-clear-all__text ng-binding']")));
     }
 }
 
