cf79b78130f767ec543f0ca19f33de6fd24f4b78
Jenkins <jenkins@stoloto.ru>
2018-06-01 16:28:58 +0300

    fix

--- src/test/java/ru/gamble/CucumberTest.java
+++ src/test/java/ru/gamble/CucumberTest.java
@@ -31,7 +31,7 @@ import java.util.concurrent.TimeUnit;
         glue = {"ru.gamble.stepdefs", "ru.sbtqa.tag.pagefactory.stepdefs"},
         features = {"src/test/resources/features/"},
         plugin= {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
-        tags = {"@NewUserRegistration_C36189, @AuthenticationOnSiteAndExit_1"})
+        tags = {"@smoke"})
 
 public class CucumberTest {
     private static final Logger LOG = LoggerFactory.getLogger(CucumberTest.class);
