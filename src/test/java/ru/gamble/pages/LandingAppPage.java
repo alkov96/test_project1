3796e11d638f09c1f98542b0e0c5941dbfb79b97
a.kovtun <alina.kovtun@stoloto.ru>
2018-06-18 13:38:06 +0300

    LandingAppDownload_C1075

--- src/test/java/ru/gamble/pages/LandingAppPage.java
+++ src/test/java/ru/gamble/pages/LandingAppPage.java
@@ -1,5 +1,14 @@
 package ru.gamble.pages;
 
+import cucumber.runtime.junit.Assertions;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,12 +16,18 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.gamble.stepdefs.CommonStepDefs;
 import ru.sbtqa.tag.pagefactory.PageFactory;
+import ru.sbtqa.tag.pagefactory.annotations.ActionTitle;
 import ru.sbtqa.tag.pagefactory.annotations.PageEntry;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
+import sun.rmi.runtime.Log;
 
+import java.io.IOException;
 import java.time.LocalTime;
+
+import static org.openqa.selenium.By.xpath;
 
 /**
  * @author p.sivak.
@@ -31,5 +46,65 @@ public class LandingAppPage extends AbstractPage {
         PageFactory.initElements(new HtmlElementDecorator(
                 new HtmlElementLocatorFactory(driver)), this);
         new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOf(header));
+    }
+
+    @ActionTitle("проверяет скачивание приложения на ios")
+    public void downloadIos() {
+        boolean flag = true;
+        WebDriver driver = PageFactory.getDriver();
+        int x, y;
+        y = driver.findElement(xpath("//div[contains(@class,'block-text_first')]//i[contains(@class,'icon-mac')]")).getLocation().getY() - 100;
+        x = driver.findElement(xpath("//div[contains(@class,'block-text_first')]//i[contains(@class,'icon-mac')]")).getLocation().getX() - 100;
+        CommonStepDefs.scrollPage(x, y);
+        WebElement iosLink = driver.findElement(xpath("//div[contains(@class,'block-text_first')]//i[contains(@class,'icon-mac')]"));
+        flag &= CommonStepDefs.goLink(iosLink, "itunes.apple.com");
+        LOG.info("Ссылка " + "itunes.apple.com" + " открылась");
+    }
+
+    @ActionTitle("проверяет скачивание приложения на android")
+    public void downloadAndroid() throws IOException {
+        WebDriver driver = PageFactory.getDriver();
+        boolean flag = true;
+        driver.findElement(xpath("//div[contains(@class,'block-text_first')]//i[contains(@class,'icon-android')]")).click();
+        if (!driver.findElement(xpath("//div[@class='modal__body modal__body_app']")).isDisplayed()) {
+            Assert.fail("Не открылся попап на скачивание приложения для андроида");
+            flag = false;
+        } else {
+            if (!driver.findElement(By.id("app_desctop_popup_link_more")).isDisplayed()) {
+                Assert.fail("На попапе нет ссылки на правила GP. нет скопки @Подробнее@");
+                flag = false;
+            } else {
+                String pattern = "play.google.com/intl/ru_ALL/about/restricted-content/gambling";
+                WebElement rules = driver.findElement(By.id("app_desctop_popup_link_more"));
+                flag &= CommonStepDefs.goLink(rules, pattern);
+            }
+            if (!driver.findElement(By.id("app_desctop_popup_btn_download")).isDisplayed()) {
+                Assert.fail("На попапе нет кнопки на скачивание");
+                flag = false;
+            } else {
+                String link = driver.findElement(By.id("app_desctop_popup_btn_download")).getAttribute("href").trim();
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpContext localContext = new BasicHttpContext();
+                HttpGet httpGet = new HttpGet(link);
+
+                LOG.info(link);
+
+                HttpResponse httpResponse = httpClient.execute(httpGet, localContext);
+                LOG.info("Начинаю скачивание приложения на Android");
+                switch (httpResponse.getStatusLine().getStatusCode()) {
+                    case 200:
+                        LOG.info("Скачивание удалось. Статус ответа = 200");
+                        break;
+                    default:
+                        LOG.info("Ошибка при скачивании! Статус ответа = " + httpResponse.getStatusLine().getStatusCode());
+                        flag = false;
+                        Assert.fail("Скачивание приложения для андроида не удалось");
+                        break;
+                }
+            }
+        }
+
+        LOG.info("Закрытие попапа на скачивание приложения");
+        driver.findElement(By.id("app_desctop_popup_close")).click();
     }
 }
