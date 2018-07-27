d3f2bbae8186cf1adb2b0ba7ef5137a40a5078ce
Jenkins <jenkins@stoloto.ru>
2018-07-27 16:15:53 +0300

    fix

--- src/test/java/ru/gamble/CucumberTest.java
+++ src/test/java/ru/gamble/CucumberTest.java
@@ -22,7 +22,7 @@ import ru.sbtqa.tag.pagefactory.PageFactory;
         glue = {"ru.gamble.stepdefs", "ru.sbtqa.tag.stepdefs.ru"},
         features = {"src/test/resources/features/"},
         plugin= {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
-        tags = {"@NewUserRegistration_C36189"})
+        tags = {"@NewUserRegistration_C361"})
 
 public class CucumberTest {
     private static final Logger LOG = LoggerFactory.getLogger(CucumberTest.class);
@@ -50,17 +50,17 @@ public class CucumberTest {
         }
     };
 
-//    @AfterClass
-//    public static void afterScenario(){
-//           WebDriver driver = PageFactory.getWebDriver();
-//           try{
-//               LOG.info("Закрываем WebDriver");
-//               driver.quit();
-//               PageFactory.dispose();
-//           }catch (Exception e){
-//               LOG.info("WebDriver уже закрыт");
-//           }
-//    }
+    @AfterClass
+    public static void afterScenario(){
+           try{
+               WebDriver driver = PageFactory.getWebDriver();
+               LOG.info("Закрываем WebDriver");
+               driver.quit();
+               PageFactory.dispose();
+           }catch (Exception e){
+               LOG.info("WebDriver уже закрыт");
+           }
+    }
 }
 
 
