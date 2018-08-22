697d7b587a066be09b745b5cc9962bbc214e85f7
a.kovtun <alina.kovtun@stoloto.ru>
2018-08-22 11:59:45 +0300

    fix

--- src/test/java/ru/gamble/pages/mainPages/AuthenticationMainPage.java
+++ src/test/java/ru/gamble/pages/mainPages/AuthenticationMainPage.java
@@ -63,7 +63,7 @@ public class AuthenticationMainPage extends AbstractPage {
     public void checkIsBalance(String keyAmount){
         BigDecimal sumBet;
         WebDriver driver = PageFactory.getWebDriver();
-        driver.navigate().refresh();
+       // driver.navigate().refresh(); пока при обновлении 404 ошибка
         waitForElementPresent(By.id("topPanelWalletBalance"), 10);
         sumBet = new BigDecimal((String) Stash.getValue(keyAmount)).setScale(2,RoundingMode.UP).negate();
         Stash.put("sumKey",sumBet.toString());
